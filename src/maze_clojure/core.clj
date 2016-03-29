086674c937af385dfbc9f3c11edbe368953f0efd
KevinBozic <bozic.kevin@gmail.com>
2016-03-29 11:45:40 -0400

    Generate maze

--- src/maze_clojure/core.clj
+++ src/maze_clojure/core.clj
@@ -1,5 +1,32 @@
 (ns maze-clojure.core
   (:gen-class))
 
+(def size 10)
+
+(defn create-rooms []
+  (vec
+    (for [row (range size)]
+      (vec
+        (for [col (range size)]
+          {:row row, :col col, :visited? false, :bottom? true, :right? true})))))
+
 (defn -main []
-  (println "Hello, World!"))
+  (let [rooms (create-rooms)]
+    
+    ;print top walls
+    (doseq [row rooms]
+      (print " _"))
+    (println)
+    
+    ;print grid
+    (doseq [row rooms]
+      (print "|")
+      (doseq [room row]
+        (if (:bottom? room)
+          (print "_")
+          (print " "))
+        (if (:right? room)
+          (print "|")
+          (print " ")))
+      (println))))
+      
